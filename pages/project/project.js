017fbf55494aaf5ba27f460f0caf1f34dea3b6b4
xyTianZhao <420315258@qq.com>
2019-09-08 21:06:39 +0800

    修复顶部tab选中屏幕外，不滚动bug

--- pages/project/project.js
+++ pages/project/project.js
@@ -12,20 +12,16 @@ Page({
      * 当前激活的当航索引 
      */
     currentActiveNavIndex: 0,
-    /** 
-     * 上一个激活的当航索引 
-     */
-    prevActiveNavIndex: -1,
-    /** 
-     * scroll-view 横向滚动条位置 
-     */
-    scrollLeft: 0
+    activeTab:null
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     that = this
+    this.setData({
+      activeTab: 'tab-item-' + this.data.currentActiveNavIndex
+    })
     wx.stopPullDownRefresh()
     that = this
     that.loadTabsData()
@@ -43,16 +39,13 @@ Page({
   },
   loadListDataByTab: function () {
     wx.showNavigationBarLoading()
-    console.log("currentActiveNavIndex:" + that.data.currentActiveNavIndex)
     var currTab = that.data.tabDatas[that.data.currentActiveNavIndex];
     var url = app.globalData.baseUrl + 'project/list/' + that.data.pageNumbers[currTab.id] + '/json?cid=' + currTab.id;
-    console.log(url)
     wx.request({
       url: url,
       success: function (res) {
         wx.hideNavigationBarLoading()
         wx.stopPullDownRefresh()
-        console.log(res)
         var resultListData = that.data.tabListDatas
         if (res.data.data.curPage == 0){
           resultListData[currTab.id] = res.data.data.datas
@@ -113,7 +106,6 @@ Page({
     if (currentIndex != nextActiveIndex) {
       this.setData({
         currentActiveNavIndex: nextActiveIndex,
-        prevActiveNavIndex: currentIndex
       });
       that.refreshTabData();
     }
@@ -124,16 +116,10 @@ Page({
    * 2、prevIndex = currentIndex  表示的是通过点击顶部的导航触发的 
    */
   swiperChange: function (e) {
-    var prevIndex = this.data.currentActiveNavIndex,
-      currentIndex = e.detail.current;
+    var currentIndex = e.detail.current;
     this.setData({
       currentActiveNavIndex: currentIndex
     });
-    if (prevIndex != currentIndex) {
-      this.setData({
-        prevActiveNavIndex: prevIndex
-      });
-    }
     this.scrollTopNav();
     that.refreshTabData()
   },
@@ -142,22 +128,8 @@ Page({
    * 1、这个地方是大致估算的 
    */
   scrollTopNav: function () {
-    /** 
-     * 当激活的当航小于4个时，不滚动 
-     */
-    if (this.data.currentActiveNavIndex <= 3 && this.data.scrollLeft >= 0) {
-      this.setData({
-        scrollLeft: 0
-      });
-    } else {
-      /** 
-       * 当超过4个时，需要判断是向左还是向右滚动，然后做相应的处理 
-       */
-      var plus = this.data.currentActiveNavIndex > this.data.prevActiveNavIndex ? 60 : -60;
-      this.setData({
-        scrollLeft: this.data.scrollLeft + plus
-      });
-    }
-    console.info(this.data.currentActiveNavIndex, this.data.prevActiveNavIndex, this.data.scrollLeft);
+    this.setData({
+      activeTab: 'tab-item-' + this.data.currentActiveNavIndex
+    })
   }
 })  
--- pages/wechatAccount/wechatAccount.js
+++ pages/wechatAccount/wechatAccount.js
@@ -12,19 +12,15 @@ Page({
      * 当前激活的当航索引 
      */
     currentActiveNavIndex: 0,
-    /** 
-     * 上一个激活的当航索引 
-     */
-    prevActiveNavIndex: -1,
-    /** 
-     * scroll-view 横向滚动条位置 
-     */
-    scrollLeft: 0
+    activeTab: null
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.setData({
+      activeTab: 'tab-item-' + this.data.currentActiveNavIndex
+    })
     that = this
     wx.stopPullDownRefresh()
     that = this
@@ -43,16 +39,13 @@ Page({
   },
   loadListDataByTab: function () {
     wx.showNavigationBarLoading()
-    console.log("currentActiveNavIndex:" + that.data.currentActiveNavIndex)
     var currTab = that.data.tabDatas[that.data.currentActiveNavIndex];
     var url = app.globalData.baseUrl + 'wxarticle/list/' + currTab.id +'/'+ that.data.pageNumbers[currTab.id] + '/json';
-    console.log(url)
     wx.request({
       url: url,
       success: function (res) {
         wx.hideNavigationBarLoading()
         wx.stopPullDownRefresh()
-        console.log(res)
         var resultListData = that.data.tabListDatas
         if (res.data.data.curPage == 0) {
           resultListData[currTab.id] = res.data.data.datas
@@ -103,31 +96,22 @@ Page({
    */
   topNavChange: function (e) {
     var nextActiveIndex = e.currentTarget.dataset.currentIndex,
-      currentIndex = this.data.currentActiveNavIndex;
+    currentIndex = this.data.currentActiveNavIndex;
     if (currentIndex != nextActiveIndex) {
       this.setData({
         currentActiveNavIndex: nextActiveIndex,
-        prevActiveNavIndex: currentIndex
       });
       that.refreshTabData();
     }
   },
   /** 
    * swiper滑动时触发 
-   * 1、prevIndex != currentIndex 表示的是用手滑动 swiper组件 
-   * 2、prevIndex = currentIndex  表示的是通过点击顶部的导航触发的 
    */
   swiperChange: function (e) {
-    var prevIndex = this.data.currentActiveNavIndex,
-      currentIndex = e.detail.current;
+    var currentIndex = e.detail.current;
     this.setData({
       currentActiveNavIndex: currentIndex
     });
-    if (prevIndex != currentIndex) {
-      this.setData({
-        prevActiveNavIndex: prevIndex
-      });
-    }
     this.scrollTopNav();
     that.refreshTabData()
   },
@@ -136,22 +120,8 @@ Page({
    * 1、这个地方是大致估算的 
    */
   scrollTopNav: function () {
-    /** 
-     * 当激活的当航小于4个时，不滚动 
-     */
-    if (this.data.currentActiveNavIndex <= 3 && this.data.scrollLeft >= 0) {
-      this.setData({
-        scrollLeft: 0
-      });
-    } else {
-      /** 
-       * 当超过4个时，需要判断是向左还是向右滚动，然后做相应的处理 
-       */
-      var plus = this.data.currentActiveNavIndex > this.data.prevActiveNavIndex ? 60 : -60;
-      this.setData({
-        scrollLeft: this.data.scrollLeft + plus
-      });
-    }
-    console.info(this.data.currentActiveNavIndex, this.data.prevActiveNavIndex, this.data.scrollLeft);
+    this.setData({
+      activeTab: 'tab-item-' + this.data.currentActiveNavIndex
+    })
   }
 })  
